68c793d7b3ffca60790d7cca17e3810ccc3b33b1
Alejandro J. Cura <alecu@protocultura.net>
2019-03-03 20:32:34 -0300

    agregada la imagen de game over

--- vyruss/images/convert-mpy.py
+++ vyruss/images/convert-mpy.py
@@ -17,6 +17,7 @@ attributes = {
   "galaga.alt8.png": (8, 8, 12, 0),
   "galaga.png": (16, 16, 12, 0),
   "ll9.png": (16, 16, 1, 0),
+  "gameover.png": (64, 20, 1, 0),
 }
 
 def grouper(iterable, n, fillvalue=None):
