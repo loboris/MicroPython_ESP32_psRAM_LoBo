e4f6660fc0fe334d560314a94331b3241777bf6b
Alejandro J. Cura <alecu@protocultura.net>
2019-10-22 23:20:46 -0300

    Extraer clase Sprites a su propio módulo

--- vyruss/python/model.py
+++ vyruss/python/model.py
@@ -1,17 +1,10 @@
-import spritelib
 import comms
+
+from sprites import Sprite, reset_sprites
 
 def audio_play(track):
     comms.send("audio_play " + track)
 
-DISABLED_FRAME = -1
-sprite_num = 1
-
-def new_sprite():
-    global sprite_num
-    sprite = spritelib.get_sprite(sprite_num)
-    sprite_num += 1
-    return sprite
 
 def calculate_direction(current, destination):
     center_delta = 128 - current
@@ -24,15 +17,8 @@ def calculate_direction(current, destination):
 
 class Scene:
     def __init__(self):
-        self.reset_sprites()
+        reset_sprites()
         self.setup()
-
-    def reset_sprites(self):
-        for n in range(1, 64):
-            sp = spritelib.get_sprite(n)
-            sp.frame = DISABLED_FRAME
-        global sprite_num
-        sprite_num = 1
 
     def step(self):
         pass
@@ -172,7 +158,7 @@ class StateResetting(FleetState):
 
     def step(self):
         StateResetting.next_state = StateEntering
-        self.fleet.reset_sprites()
+        reset_sprites()
         self.fleet.setup()
 
 class StateAttacking(FleetState):
@@ -264,58 +250,6 @@ class StateEntering(FleetState):
             else:
                 self.fleet.change_state()
 
-class Sprite:
-    def __init__(self, strip, x=0, y=0, frame=DISABLED_FRAME):
-        self._sprite = new_sprite()
-        self.strip = strip
-        self.x = x
-        self.y = y
-        self.frame = frame
-
-    @property
-    def x(self):
-        return self._sprite.x
-    
-    @x.setter
-    def x(self, value):
-        self._sprite.x = value
-
-    @property
-    def y(self):
-        return self._sprite.y
-    
-    @y.setter
-    def y(self, value):
-        self._sprite.y = value
-
-    @property
-    def width(self):
-        return spritelib.sprite_width(self._sprite)
-
-    @property
-    def height(self):
-        return spritelib.sprite_height(self._sprite)
-
-    def strip(self, strip_number):
-        self._sprite.image_strip = strip_number
-    strip = property(None, strip)
-
-    @property
-    def frame(self):
-        return self._sprite.frame
-    
-    @frame.setter
-    def frame(self, value):
-        self._sprite.frame = value
-
-    def collision(self, targets):
-        for target in targets:
-            other = target
-            if (self.x < other.x + other.width and
-                self.x + self.width > other.x and
-                self.y < other.y + other.height and
-                self.y + self.height > other.y):
-                return target
 
 class StarFighter(Sprite):
     def __init__(self):
@@ -350,7 +284,7 @@ class Laser(Sprite):
 
     def finish(self):
         self.enabled = False
-        self.frame = DISABLED_FRAME
+        self.disable()
 
     def step(self):
         LASER_SPEED = 6
@@ -374,7 +308,7 @@ class BaddieExploding(Sprite):
     def step(self):
         self.frame += 1
         if self.frame == 9:
-            self.frame = DISABLED_FRAME
+            self.disable()
             self.finished = True
 
 class Baddie(Sprite):
---
+++ vyruss/python/sprites.py
@@ -0,0 +1,75 @@
+import spritelib
+
+DISABLED_FRAME = -1
+sprite_num = 1
+
+
+def new_sprite():
+    global sprite_num
+    sprite = spritelib.get_sprite(sprite_num)
+    sprite_num += 1
+    return sprite
+
+def reset_sprites():
+    for n in range(1, 64):
+        sp = spritelib.get_sprite(n)
+        sp.frame = DISABLED_FRAME
+    global sprite_num
+    sprite_num = 1
+
+
+class Sprite:
+    def __init__(self, strip, x=0, y=0, frame=DISABLED_FRAME):
+        self._sprite = new_sprite()
+        self.strip = strip
+        self.x = x
+        self.y = y
+        self.frame = frame
+
+    def disable(self):
+        self.frame = DISABLED_FRAME
+
+    @property
+    def x(self):
+        return self._sprite.x
+    
+    @x.setter
+    def x(self, value):
+        self._sprite.x = value
+
+    @property
+    def y(self):
+        return self._sprite.y
+    
+    @y.setter
+    def y(self, value):
+        self._sprite.y = value
+
+    @property
+    def width(self):
+        return spritelib.sprite_width(self._sprite)
+
+    @property
+    def height(self):
+        return spritelib.sprite_height(self._sprite)
+
+    def strip(self, strip_number):
+        self._sprite.image_strip = strip_number
+    strip = property(None, strip)
+
+    @property
+    def frame(self):
+        return self._sprite.frame
+    
+    @frame.setter
+    def frame(self, value):
+        self._sprite.frame = value
+
+    def collision(self, targets):
+        for target in targets:
+            other = target
+            if (self.x < other.x + other.width and
+                self.x + self.width > other.x and
+                self.y < other.y + other.height and
+                self.y + self.height > other.y):
+                return target
