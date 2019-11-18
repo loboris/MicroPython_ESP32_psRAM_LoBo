2f74f1e7f5e9164ceb38c75c2d2d6ae1316ef3ab
Juan Pedro Fisanotti <fisadev@gmail.com>
2019-11-17 21:13:21 -0300

    Refactor: have a director able to handle most of the 'global' stuff

---
+++ vyruss/python/director.py
@@ -0,0 +1,85 @@
+import utime
+
+import comms
+import sprites
+
+
+try:
+    from remotepov import update
+except:
+    import povdisplay
+    povdisplay.init(PIXELS, imagenes.palette_pal)
+    update = lambda: None
+    if DEBUG:
+        print("setting up fan debug")
+        import uctypes
+        debug_buffer = uctypes.bytearray_at(povdisplay.getaddress(999), 32*16)
+        next_loop = 1000
+        def update():
+            global next_loop
+            now = utime.ticks_ms()
+            if utime.ticks_diff(next_loop, now) < 0:
+                next_loop = utime.ticks_add(now, 1000)
+                comms.send(b"debug", debug_buffer)
+
+
+class Director:
+    BUTTON_LEFT = 1
+    BUTTON_RIGHT = 2
+    BUTTON_UP = 4
+    BUTTON_DOWN = 8
+    BUTTON_A = 16
+    BUTTON_B = 32
+    BUTTON_C = 64
+    BUTTON_D = 128
+
+    def __init__(self):
+        self.scene_stack = []
+        self.buttons = 0
+        self.last_buttons = 0
+
+    def push(self, scene):
+        self.scene_stack.append(scene)
+
+    def pop(self):
+        return self.scene_stack.pop()
+
+    def register_strip(self, sprite_index, image):
+        sprites.set_imagestrip(sprite_index, image)
+
+    def is_pressed(self, button):
+        return bool(button & self.buttons)
+
+    def was_pressed(self, button):
+        return bool(button & self.buttons) and not bool(button & self.last_buttons)
+
+    def was_released(self, button):
+        return not bool(button & self.buttons) and bool(button & self.last_buttons)
+
+    def run(self):
+        while True:
+            scene = self.scene_stack[-1]
+            next_loop = utime.ticks_add(utime.ticks_ms(), 30)
+
+            val = comms.receive(1)
+            if val is not None:
+                self.buttons = val[0]
+
+            try:
+                scene.step()
+            except StopIteration:
+                pass
+
+            self.last_buttons = self.buttons
+
+            # TODO check this hack
+            update()
+
+            delay = utime.ticks_diff(next_loop, utime.ticks_ms())
+            if delay > 0:
+                utime.sleep_ms(delay)
+            else:
+                print("odelay:", delay)
+
+
+director = Director()
--- vyruss/python/main.py
+++ vyruss/python/main.py
@@ -1,32 +1,13 @@
+from urandom import seed
 import utime
 
-import comms
-import sprites
-import model
-
+from director import director
 import imagenes
+import model
 
 PIXELS = 54
 
 DEBUG = True
-
-try:
-    from remotepov import update
-except:
-    import povdisplay
-    povdisplay.init(PIXELS, imagenes.palette_pal)
-    update = lambda: None
-    if DEBUG:
-        print("setting up fan debug")
-        import uctypes
-        debug_buffer = uctypes.bytearray_at(povdisplay.getaddress(999), 32*16)
-        next_loop = 1000
-        def update():
-            global next_loop
-            now = utime.ticks_ms()
-            if utime.ticks_diff(next_loop, now) < 0:
-                next_loop = utime.ticks_add(now, 1000)
-                comms.send(b"debug", debug_buffer)
 
 #gameover = sprites.get_sprite(0)
 #gameover.image_strip = 6
@@ -35,102 +16,31 @@ except:
 #gameover.x = -32
 #gameover.y = 2
 
-# init images
-sprites.set_imagestrip(0, imagenes.galaga_png)
-sprites.set_imagestrip(1, imagenes.numerals_png)
-sprites.set_imagestrip(3, imagenes.disparo_png)
-sprites.set_imagestrip(4, imagenes.ll9_png)
-#sprites.set_imagestrip(4, imagenes._00_galaga_png)
-#sprites.set_imagestrip(4, imagenes.crawling_png)
-sprites.set_imagestrip(5, imagenes.explosion_png)
-sprites.set_imagestrip(6, imagenes.explosion_nave_png)
-sprites.set_imagestrip(10, imagenes.tierra_flat_png)
-sprites.set_imagestrip(11, imagenes.marte_flat_png)
-sprites.set_imagestrip(12, imagenes.jupiter_flat_png)
-sprites.set_imagestrip(13, imagenes.saturno_flat_png)
-sprites.set_imagestrip(14, imagenes.sves_flat_png)
-sprites.set_imagestrip(15, imagenes.ventilastation_flat_png)
-sprites.set_imagestrip(16, imagenes.tecno_estructuras_flat_png)
 
-def reset_game():
+def main():
+    # TODO: para hacerlo más random esto debería suceder después de que
+    # le jugadore toca el botón para iniciar la partida
+    seed(utime.ticks_ms())
 
-    global scene
-    scene = model.Fleet()
+    # init images
+    director.register_strip(0, imagenes.galaga_png)
+    director.register_strip(1, imagenes.numerals_png)
+    director.register_strip(3, imagenes.disparo_png)
+    director.register_strip(4, imagenes.ll9_png)
+    #director.register_strip(4, imagenes._00_galaga_png)
+    #director.register_strip(4, imagenes.crawling_png)
+    director.register_strip(5, imagenes.explosion_png)
+    director.register_strip(6, imagenes.explosion_nave_png)
+    director.register_strip(10, imagenes.tierra_flat_png)
+    director.register_strip(11, imagenes.marte_flat_png)
+    director.register_strip(12, imagenes.jupiter_flat_png)
+    director.register_strip(13, imagenes.saturno_flat_png)
+    director.register_strip(14, imagenes.sves_flat_png)
+    director.register_strip(15, imagenes.ventilastation_flat_png)
+    director.register_strip(16, imagenes.tecno_estructuras_flat_png)
 
-button_was_down = False
-reset_was_down = False
+    director.push(model.VyrusGame())
+    director.run()
 
-def process_input(b):
-    left =  bool(b & 1)
-    right = bool(b & 2)
-    up =    bool(b & 4)
-    down =  bool(b & 8)
-    boton = bool(b & 16)
-    accel = bool(b & 32)
-    decel = bool(b & 64)
-    reset = bool(b & 128)
-
-    global reset_was_down
-    if not reset_was_down and reset:
-        reset_game()
-    reset_was_down = reset
-
-    if up and left:
-        direction = "↖"
-    elif up and right:
-        direction = "↗"
-    elif down and left:
-        direction = "↙"
-    elif down and right:
-        direction = "↘"
-    elif up:
-        direction = "↑"
-    elif down:
-        direction = "↓"
-    elif left:
-        direction = "←"
-    elif right:
-        direction = "→"
-    else:
-        direction = " "
-
-    scene.heading(up, down, left, right)
-
-    global button_was_down
-    if not button_was_down and boton:
-        scene.fire()
-    button_was_down = boton
-            
-    scene.accel(accel, decel)
-
-    #text = "\r{0} {2} {1} {3} {4}   ".format(direction, boton, int(nave.x), decel, accel)
-    #sock_send(bytes(text, "utf-8"))
-    #print(text, end="")
-
-
-
-def game_loop():
-    last_val = None
-    counter = 0    
-    reset_game()
- 
-    while True:
-        next_loop = utime.ticks_add(utime.ticks_ms(), 30)
-
-        val = comms.receive(1)
-        if val is not None:
-            process_input(val[0])
-            last_val = val[0]
-        elif last_val is not None:
-            process_input(last_val)
-
-        scene.step()
-
-        update()
-        delay = utime.ticks_diff(next_loop, utime.ticks_ms())
-        if delay > 0:
-            utime.sleep_ms(delay)
-        else:
-            print("odelay:", delay)
-
-game_loop()
+if __name__ == '__main__':
+    main()
