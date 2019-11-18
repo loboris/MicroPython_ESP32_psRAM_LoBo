33617c453b13746d8104b81ab739564ca13ea745
Juan Pedro Fisanotti <fisadev@gmail.com>
2019-11-17 21:47:03 -0300

    Scene in a new scene module, and setup() replaced with init()

---
+++ vyruss/python/scene.py
@@ -0,0 +1,28 @@
+import utime
+
+from sprites import reset_sprites
+
+
+class Scene:
+    def __init__(self):
+        reset_sprites()
+        self.pending_calls = []
+
+    def call_later(self, delay, callable):
+        when = utime.ticks_add(utime.ticks_ms(), delay)
+        self.pending_calls.append((when, callable))
+        self.pending_calls.sort()
+
+    def scene_step(self):
+        self.step()
+        now = utime.ticks_ms()
+        while self.pending_calls:
+            when, callable = self.pending_calls[0]
+            if utime.ticks_diff(when, now) <= 0:
+                self.pending_calls.pop(0)
+                callable()
+            else:
+                break
+
+    def step(self):
+        pass
