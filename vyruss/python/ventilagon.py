8348e7cae4fcae56d599c30f934f88c9120b9a62
Alejandro J. Cura <alecu@protocultura.net>
2019-11-21 14:40:55 -0300

    Prueba de la perspectiva de planeta

--- vyruss/python/ventilagon.py
+++ vyruss/python/ventilagon.py
@@ -1,1 +1,35 @@
-# FIXME
+from director import director
+from scene import Scene
+from sprites import Sprite, reset_sprites
+
+
+def make_me_a_planet(n):
+    planet = Sprite()
+    planet.set_strip(n)
+    planet.set_perspective(0)
+    planet.set_x(0)
+    y = 54 - planet.height()
+    planet.set_y(y)
+    return planet
+
+
+class VentilagonGame(Scene):
+    def on_enter(self):
+        self.planet_id = 3
+        self.planet = make_me_a_planet(10)
+        self.planet.set_frame(0)
+        self.call_later(30, self.animate)
+    
+    def animate(self):
+        new_y = (self.planet.y() + 1) % 256
+        self.planet.set_y(new_y)
+        self.call_later(30, self.animate)
+
+    def step(self):
+        if director.was_pressed(director.BUTTON_A):
+            self.planet_id = (self.planet_id + 1) % 4
+            self.planet.set_strip(self.planet_id + 10)
+
+        if director.was_pressed(director.BUTTON_D):
+            director.pop()
+            raise StopIteration()
